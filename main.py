5419456b725e307e3f16e06cf83285f94df7ead4
Danil <danil1198@yandex.ru>
2020-05-31 02:42:57 +0300

    управление персонажем, бег без остановки

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ class Board(pygame.sprite.Sprite):
 
 
 class Wall(pygame.sprite.Sprite):
-    def __init__(self, Board, left, top, height, width):
+    def __init__(self, Board_cs, left, top, height, width):
         super().__init__(Walls)
         self.add(Walls)
-        self.cell_size = Board.cell_size
+        self.cell_size = Board_cs
         self.height = height * self.cell_size
         self.width = width * self.cell_size
         if left == 1:
@@ -43,47 +43,59 @@ class Wall(pygame.sprite.Sprite):
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, cell_size, x, y):
-        super().__init__()
+        super().__init__(all_sprites)
         self.cell_size = cell_size
         self.left = x * self.cell_size
         self.top = y * self.cell_size
+        self.way = " "
         self.rect = pygame.Rect((self.left, self.top), (self.cell_size, self.cell_size))
-        self.Surface = pygame.Surface((self.cell_size, self.cell_size))
+        self.image = pygame.Surface((self.cell_size, self.cell_size))
+        self.image.fill(RED)
 
-    def render(self, Surface):
-        self.Surface.fill(RED)
-        Surface.blit(self.Surface, self.rect)
+    def move(self, new_way):
+        global try_way
+        old_way = self.way
+        if try_way % 2 == 1:
+            self.way = new_way
+            try_way = 1
+        if self.way == "up":
+            self.rect.move_ip(0, -11)
+            if pygame.sprite.spritecollideany(self, Walls):
+                self.rect.move_ip(0, 11)
+                self.way = old_way
+        elif self.way == "down":
+            self.rect.move_ip(0, 11)
+            if pygame.sprite.spritecollideany(self, Walls):
+                self.rect.move_ip(0, -11)
+                self.way = old_way
+        elif self.way == "left":
+            self.rect.move_ip(-11, 0)
+            if pygame.sprite.spritecollideany(self, Walls):
+                self.rect.move_ip(11, 0)
+                self.way = old_way
+        elif self.way == "right":
+            self.rect.move_ip(11, 0)
+            if pygame.sprite.spritecollideany(self, Walls):
+                self.rect.move_ip(-11, 0)
+                self.way = old_way
+        try_way += 1
 
-    def move(self, motion):
-        if motion == "up":
-            self.rect.move_ip(0, -self.cell_size)
-            if pygame.sprite.spritecollideany(self, Walls):
-                self.rect.move_ip(0, self.cell_size)
-        elif motion == "down":
-            self.rect.move_ip(0, self.cell_size)
-            if pygame.sprite.spritecollideany(self, Walls):
-                self.rect.move_ip(0, -self.cell_size)
-        elif motion == "left":
-            self.rect.move_ip(-self.cell_size, 0)
-            if pygame.sprite.spritecollideany(self, Walls):
-                self.rect.move_ip(self.cell_size, 0)
-        elif motion == "right":
-            self.rect.move_ip(self.cell_size, 0)
-            if pygame.sprite.spritecollideany(self, Walls):
-                self.rect.move_ip(-self.cell_size, 0)
 
 
 sc_size = sc_width, sc_height = 1024, 640
 screen = pygame.display.set_mode(sc_size)
 
 clock = pygame.time.Clock()
-fps = 24
+fps = 30
 
 BLACK, WHITE, RED, GREEN, BLUE = (0, 0, 0), (255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255)
 board = Board(31, 28, 22)
 
-player = Player(board.cell_size, 13, 13)
-motion = "stop"
+all_sprites = pygame.sprite.Group()
+
+player = Player(board.cell_size, 1, 1)
+new_way = " "
+try_way = 1
 
 Walls = pygame.sprite.Group()
 wall_list = {(6, 2, 2, 2), (6, 23, 5, 2), (1, 1, 1, 28), (9, 25, 2, 2), (6, 23, 5, 2),
@@ -96,7 +108,7 @@ wall_list = {(6, 2, 2, 2), (6, 23, 5, 2), (1, 1, 1, 28), (9, 25, 2, 2), (6, 23, 
              (18, 20, 8, 2), (18, 23, 5, 6), (21, 17, 2, 3), (21, 10, 2, 3), (21, 14, 3, 2),
              (24, 11, 2, 8), (24, 23, 2, 4), (27, 23, 3, 4), (27, 17, 3, 5), (27, 14, 4, 2), (27, 8, 3, 5)}
 for w in wall_list:
-    Wall(board, w[0], w[1], w[2], w[3])
+    Wall(board.cell_size, w[0], w[1], w[2], w[3])
 
 running = True
 while running:
@@ -107,18 +119,15 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                motion = "up"
+                new_way = "up"
             elif event.key == pygame.K_s:
-                motion = "down"
+                new_way = "down"
             elif event.key == pygame.K_a:
-                motion = "left"
+                new_way = "left"
             elif event.key == pygame.K_d:
-                motion = "right"
-        elif event.type == pygame.KEYUP:
-            if event.key in [pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d]:
-                motion = "stop"
-    player.move(motion)
-    player.render(board.Surface)
+                new_way = "right"
+    player.move(new_way)
+    all_sprites.draw(board.Surface)
     screen.blit(board.Surface, (board.left, board.top))
     clock.tick(fps)
     pygame.display.flip()
